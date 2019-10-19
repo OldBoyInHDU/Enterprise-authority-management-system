a920f59fe7336d5f45e1b9aca991a08db7a25a83
HuangHeng <973157930@qq.com>
2019-10-19 14:46:38 +0800

    日期转换问题解决+新建产品功能完成

--- eams_ssm/eams_ssm_dao/src/main/java/com/ruki/eams/dao/IProductDao.java
+++ eams_ssm/eams_ssm_dao/src/main/java/com/ruki/eams/dao/IProductDao.java
@@ -1,6 +1,7 @@
 package com.ruki.eams.dao;
 
 import com.ruki.eams.domain.Product;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
@@ -12,4 +13,7 @@ import java.util.List;
 public interface IProductDao {
     @Select("select * from product")
     List<Product> findAll() throws Exception;
+
+    @Insert("insert into product(id,productNum,productName,cityName,departureTime,productPrice,productDesc,productStatus) values(UUID(),#{productNum},#{productName},#{cityName},#{departureTime},#{productPrice},#{productDesc},#{productStatus})")
+    void save(Product product) throws Exception;
 }
--- eams_ssm/eams_ssm_web/src/main/java/com/ruki/eams/controller/ProductController.java
+++ eams_ssm/eams_ssm_web/src/main/java/com/ruki/eams/controller/ProductController.java
@@ -16,12 +16,29 @@ public class ProductController {
     @Autowired
     private IProductService productService;
 
+    /**
+     * 产品查询
+     * @return
+     * @throws Exception
+     */
     @RequestMapping("/findAll.do")
     public ModelAndView findAll() throws Exception {
         ModelAndView mv = new ModelAndView();
         List<Product> products = productService.findAll();
         mv.addObject("productList",products);
-        mv.setViewName("product-list");
+        mv.setViewName("product-list1");
         return mv;
     }
+
+    /**
+     * 保存商品
+     * @param product
+     * @return
+     */
+    @RequestMapping("/save.do")
+    public String save(Product product) throws Exception {
+        productService.save(product);
+        return "redirect:findAll.do";
+    }
+
 }
